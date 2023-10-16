7c4461437edd35a7377ad2be82168483eb7953b5
Vrn24 <112547439+Vrn24@users.noreply.github.com>
2023-10-16 15:47:54 +0530

    Add files via upload

---
+++ Number Triangle.cpp
@@ -0,0 +1,21 @@
+/*22070123129*/
+/*Varun Pagote*/
+/*Experiment 8*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int ary[5],i;
+    cout<<"Enter the numbers : ";
+    for(i=0;i<5;i++)
+    {
+        cin>>ary[i];
+    }
+    cout<<endl<<"The numbers are : ";
+    for(i=0;i<5;i++)
+    {
+        cout<<ary[i]<<endl;
+    }
+    cout<<endl<<"The ";
+}
